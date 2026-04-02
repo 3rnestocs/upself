dc5db5b9db35431e78b070708696bf60f77ac969
3rnestocs <3rnestocs@gmail.com>
2026-04-02 13:39:50 -0400

    Initial Commit

---
+++ UpSelfTests/UpSelfTests.swift
@@ -0,0 +1,16 @@
+//
+//  UpSelfTests.swift
+//  UpSelfTests
+//
+//  Created by Ernesto Contreras on 2/4/26.
+//
+
+import Testing
+
+struct UpSelfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
